9a85ae7798580f1ea6459b30f340da4f61f18cdf
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2024-08-05 14:55:27 -0300

    Projeto com SpringBoot iniciado.

---
+++ src/main/java/engtelecom/std/apibaralho/ApiBaralhoApplication.java
@@ -0,0 +1,13 @@
+package engtelecom.std.apibaralho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBaralhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBaralhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/engtelecom/std/apibaralho/ApiBaralhoApplicationTests.java
@@ -0,0 +1,13 @@
+package engtelecom.std.apibaralho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBaralhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
