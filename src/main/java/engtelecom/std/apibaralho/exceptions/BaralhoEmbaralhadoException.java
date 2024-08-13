33da31be97dceebddd4c64295d87ff5d1150f624
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2024-08-12 23:16:23 -0300

    Criada a exceção para quando o baralho já estiver embaralhado.

---
+++ src/main/java/engtelecom/std/apibaralho/exceptions/BaralhoEmbaralhadoException.java
@@ -0,0 +1,7 @@
+package engtelecom.std.apibaralho.exceptions;
+
+public class BaralhoEmbaralhadoException extends RuntimeException {
+    public BaralhoEmbaralhadoException(String uuid){
+        super("OOPS! O baralho com o identificador: \"" + uuid + "\" já está embaralhado!");
+    }
+}
